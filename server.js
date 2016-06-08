fe0a5df1f4082df7ccbea10485646f557f7c4f15
Tobia Moretti <moretti.tobia@live.com>
2016-06-08 20:09:47 +0000

    working solution

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+var dateFormat = require('dateformat');
+var express = require('express');
+
+var app = express();
+
+app.get('/:date',function(req,res) {
+	var json = undefined;
+	var date = new Date(req.params.date);
+	if (isNaN(date.valueOf())) {
+		json = "{" + "\"unix\":" + null + " , " + "\"natural\":" + null + "}";
+	}
+	else {
+		json = "{" + 
+			   "\"unix\": \"" + date.getTime() + "\" , " + 
+			   "\"natural\": \"" + dateFormat(date, "mmmm dd, yyyy") + "\"" + 
+			   "}";
+	}	
+	res.end(JSON.stringify(JSON.parse(json)));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
